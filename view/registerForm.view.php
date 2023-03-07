cfdb19f9e222a7f60b77d17d35b5e116fa0fb0ec
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-07 11:09:25 +0100

    methode pdo

--- view/registerForm.view.php
+++ view/registerForm.view.php
@@ -2,11 +2,30 @@
 require_once './inc/fonctions.php';
 require_once './inc/pdo.php';
 
+// liaison des champs
 $pseudo = $_POST['pseudo'];
 $email = $_POST['email'];
 $pwd = $_POST['pwd'];
 
-$sql = "INSERT INTO user ('id_user','login','pwd','created_at','email') VALUES (null, '$pseudo','$pwd','','$email')";
+// création requête
+$sql = "INSERT INTO user ('id_user','login','pwd','created_at','email') VALUES (null, ':pseudo',':pwd','',':email')";
+$resultat = $conn->prepare($sql);
+$resultat ->bindParam(':pseudo', $pseudo);
+$resultat ->bindParam(':pwd', $pwd);
+$resultat ->bindParam(':email', $email);
+
+
+//exécution requête
+if ($resultat->execute()) :
+echo 'Les données ont été enregistrées avec succès';
+
+else :
+    echo "Erreur : ". $erreur->getMessage();
+
+endif;
+
+//fermer connexion
+$conn = null;
 
 ?>
 
