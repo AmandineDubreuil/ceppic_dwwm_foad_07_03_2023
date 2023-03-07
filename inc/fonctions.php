0e630e2e83e0e34952d780f422eca2b670f258e3
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-07 15:48:41 +0100

    sécurisation des données

--- inc/fonctions.php
+++ inc/fonctions.php
@@ -17,3 +17,109 @@ function dd($valeur)
     echo "</pre>";
     die();
 }
+/****************** 
+FORMULAIRES
+ ******************/
+
+
+/**
+ * checkXSSPostValue
+ * 
+ * Cette fonction sert à vérifier l'intégrité de la variable post transmise (faille XSS).
+ * 
+ * @param  mixed $postEntrie : entrée de formulaire à vérifier
+ * @return mixed variable post nettoyée de ses tags
+ */
+function checkXSSPostValue($postEntrie)
+{
+    $postEntrie = strip_tags($postEntrie);
+    return $postEntrie;
+}
+
+// vérif si champs est vide
+
+
+/**
+ * checkEmptyValue
+ *
+ * Cette fonction permet de vérifier si un champs obligatoire est vide. 
+ * 
+ * @param  mixed $postEntrie : entrée de formulaire à vérifier
+ * @param  string $key : name HTML(index post de mon entrée)
+ * @param  array $error : tableau d'erreurs de mon formulaire
+ * @return array $error : retourne le tableau d'erreurs modifié si le champs est vide
+ * @version 1.0.0
+ * @author Cemoi
+ */
+function checkEmptyValue($postEntrie, $key, $error)
+{
+    if (empty($postEntrie)) :
+        $error[$key] = "Le champ $key est vide.";
+
+    endif;
+    return $error;
+}
+
+//validation mail
+
+/**
+ * checkEmail
+ * 
+ * Cette fonction permet de vérifier si le format de l'e-mail est valide.
+ *
+ * @param  string $emailEntrie : champs e-mail à vérifier
+ * @param  string $key :name HTML(index post de mon entrée)
+ * @param  array $error : tableau d'erreurs de mon formulaire
+ * @return array $error : retourne le tableau d'erreurs modifié si le champs est vide
+ */
+function checkEmail($postEntrie, $key, $error)
+{
+    if (!filter_var($postEntrie, FILTER_VALIDATE_EMAIL)) :
+        $error[$key] = 'L\'e-mail est invalide".';
+        return $error;
+    endif;
+}
+
+/**
+ * checkPwdValid
+ * 
+ * Cette fonction permet de savoir si le mot de passe correspond aux critères de sécurité.
+ *
+ * @param  string $postEntrie : champs e-mail à vérifier
+ * @param  string $key :name HTML(index post de mon entrée)
+ * @param  array $error : tableau d'erreurs de mon formulaire
+ * @return array $error : retourne le tableau d'erreurs modifié si le champs est vide
+ */
+function checkPwdValid($postEntrie, $key, $error)
+{
+    $password_regex = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/";
+
+    if (!preg_match($password_regex, $postEntrie)) :
+        $error[$key] = 'Le mot de passe doit comporter au moins 8 caractères, une lettre majuscule, une lettre minuscule, un chiffre et un caractère spécial.';
+    return $error;
+
+    endif;
+}
+
+/**
+ * checkPwdConfirm
+ *
+ * Cette fonction permet de vérifier si les mots de passe sont identiques 
+ * 
+ * @param  mixed $postPwd : entrée de formulaire à vérifier
+ *  @param  mixed $postConfPwd : entrée de formulaire à vérifier
+
+ * @param  string $key : name HTML(index post de mon entrée)
+ * @param  array $error : tableau d'erreurs de mon formulaire
+ * @return array $error : retourne le tableau d'erreurs modifié si les mots de passe ne sont pas identiques
+ * @version 1.0.0
+ * @author Cemoi
+ */
+function checkPwdConfirm($postPwd, $postConfPwd, $key, $error)
+{
+    if ($postPwd !== $postConfPwd) :
+        $error[$key] = "Les deux mots de passe ne sont pas identiques.";
+    return $error;
+
+    endif;
+}
--- view/registerForm.view.php
+++ view/registerForm.view.php
@@ -3,28 +3,52 @@ require_once './inc/fonctions.php';
 require_once './inc/pdo.php';
 require_once './partials/head.php';
 
-// liaison des champs
+$error = [];
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') :
+
+    //traitement des failles XSS
+    foreach ($_POST as $key => $value) :
+
+        $_POST[$key] = checkXSSPostValue($value);
+
+        // vérifie champs obligatoires vides
+        $error = checkEmptyValue($value, $key, $error);
+    endforeach;
+
+    // liaison des champs
     $pseudo = $_POST['pseudo'];
+
     $email = $_POST['email'];
-    //$date = Date('yy-m-d H:i:s');
+    //validation format e-mail
+    $error = checkEmail($_POST['email'], 'email', $error);
+
+
     $pwd = $_POST['pwd'];
+    //validation format mdp
+    $error = checkPwdValid($_POST['pwd'], 'pwd', $error);
+    // validation confirmation mdp
+    $error = checkPwdConfirm($_POST['pwd'], $_POST['confPwd'], 'confPwd', $error);
+    // hachage mdp
     $pwd = password_hash($pwd, PASSWORD_DEFAULT);
-    // dd($pwd);
 
-    //  if (!isset($email)) :
-    // création requête
-    $sql = "INSERT INTO user (login, pwd,created_at,email) VALUES (:pseudo,:pwd,now(),:email)";
-    $resultat = $conn->prepare($sql);
-    $resultat->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
-    $resultat->bindValue(':pwd', $pwd, PDO::PARAM_STR);
-    $resultat->bindValue(':email', $email, PDO::PARAM_STR);
-    $resultat->execute();
+    if (empty($error)) : ?>
+        <div class="reussi">Votre compte est bien enregistré !</div>
 
-//else :
-//  echo 'L\'e-mail est déjà connu.';
+<?php
 
-// endif;
+
+        // création requête
+        $sql = "INSERT INTO user (login, pwd,created_at,email) VALUES (:pseudo,:pwd,now(),:email)";
+        $resultat = $conn->prepare($sql);
+        $resultat->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
+        $resultat->bindValue(':pwd', $pwd, PDO::PARAM_STR);
+        $resultat->bindValue(':email', $email, PDO::PARAM_STR);
+        $resultat->execute();
+    else :
+        dbug($error);
+    endif;
+
 endif;
 ?>
 
@@ -32,18 +56,22 @@ endif;
 <form method="post" class="enregistrement">
     <div>
         <label for="pseudo">Pseudo</label>
+        <span class="error"><?= isset($error['pseudo']) ? $error['pseudo'] : "" ?></span>
         <input type="text" name="pseudo" id="pseudo">
     </div>
     <div>
         <label for="email">E-mail</label>
+        <span class="error"><?= isset($error['email']) ? $error['email'] : "" ?></span>
         <input type="email" name="email" id="email" required>
     </div>
     <div>
         <label for="pwd">Mot de passe</label>
+        <span class="error"><?= isset($error['pwd']) ? $error['pwd'] : "" ?></span>
         <input type="password" name="pwd" id="pwd" required>
     </div>
     <div>
         <label for="confPwd">Confirmation du Mot de passe</label>
+        <span class="error"><?= isset($error['confPwd']) ? $error['confPwd'] : "" ?></span>
         <input type="password" name="confPwd" id="confPwd" required>
     </div>
 
