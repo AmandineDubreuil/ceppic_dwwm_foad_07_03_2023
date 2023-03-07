efc04b842b312d933f2e35de680d20444b14966e
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-07 10:28:52 +0100

    add head

---
+++ partials/head.php
@@ -0,0 +1,38 @@
+<?php session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+
+<body>
+    <header class="header">
+        <h1>Films</h1>
+        <nav>
+            <ul>
+                <?php
+                if (isset($_SESSION['login'])) {
+                ?>
+
+                    <li><a href="./login/deconnexion.php">Deconnexion</a></li>
+                <?php
+                } else {
+                ?>
+                    <li><a href="./login/index.php">Connexion</a></li>
+                <?php
+                }
+                ?>
+            </ul>
+        </nav>
+    </header>
+    <main>
