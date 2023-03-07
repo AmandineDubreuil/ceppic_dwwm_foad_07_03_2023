a17fdd34199aaaf004aa818add1a838f539f58d3
AmandineDubreuil <Amandine.dubreuil@hotmail.com>
2023-03-07 10:35:20 +0100

    add index.view

---
+++ view/index.view.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once './inc/pdo.php';
+require_once './partials/head.php';
+
+?>
+
+
+<div>
+Bienvenue sur le super site de NeetFix
+</div>
+
+
+
+
+
+
+
+<?php
+require_once './partials/foot.php';
